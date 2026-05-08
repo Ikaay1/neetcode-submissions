55adb428d00a9428a477f986832e2abd259d4779
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-05-08 09:15:41 -0500

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,28 @@
+from sortedcontainers import SortedList
+
+class TimeMap:
+
+    def __init__(self):
+        self.time_map = defaultdict(SortedList)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.time_map[key].add((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        values = self.time_map[key]
+        left = 0
+        right = len(values)-1
+
+        while left <= right:
+            mid = (left+right)//2
+
+            if values[mid][0] <= timestamp:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        if right < 0:
+            return ""
+        
+        return values[right][1]
+        
