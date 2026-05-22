113898f398c3bc2d998a3bb1df9867f239412925
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-05-22 08:40:07 -0500

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,13 @@
+from functools import cache
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        @cache
+        def dp(index):
+            if index >= len(nums):
+                return 0
+
+            return max(nums[index] + dp(index+2), dp(index+1))
+        
+        return dp(0)
