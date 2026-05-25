ab5031942495fded9a99d73392a54e5168fd77f8
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-05-25 09:21:51 -0500

    Add: partition-equal-subset-sum - submission-2

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-2.py
@@ -0,0 +1,20 @@
+from functools import cache
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        @cache
+        def recurse(index, cur_sum):
+            if cur_sum == total-cur_sum:
+                return True
+            
+            if index == len(nums):
+                return False
+
+            if recurse(index+1, cur_sum + nums[index]) or recurse(index+1, cur_sum):
+                return True
+            
+            return False
+        
+        total = sum(nums)
+        return recurse(0, 0)
