c1d365fc25cb7e08d90340e2142830f43a3e1978
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-06-01 06:54:27 -0500

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        
+        @cache
+        def dp(row, col):
+
+            if row == m-1 and col == n-1:
+                return 1
+
+            if row < 0 or row == m or col < 0 or col == n:
+                return 0
+
+            return dp(row+1, col) + dp(row, col+1)
+        
+        return dp(0, 0)
