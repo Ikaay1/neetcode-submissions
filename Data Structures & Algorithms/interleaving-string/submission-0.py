f3786919baa33b3aab85cf9dc8cd24a02cc5b462
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-06-03 20:43:13 -0500

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.py
@@ -0,0 +1,24 @@
+from functools import cache
+
+class Solution:
+    
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        
+        @cache
+        def dp(index1, index2, index3):
+
+            if index3 == len(s3):
+                if index1 < len(s1) or index2 < len(s2):
+                    return False
+                return True
+
+            res = False
+            if index1 < len(s1) and s1[index1] == s3[index3]:
+                res = res or dp(index1+1, index2, index3+1)
+            
+            if index2 < len(s2) and s2[index2] == s3[index3]:
+                res = res or dp(index1, index2+1, index3+1)
+            
+            return res
+        
+        return dp(0, 0, 0)
