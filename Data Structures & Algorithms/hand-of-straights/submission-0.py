1eb18338a282adf14877041ec821b369b7592b3a
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-06-06 17:11:16 -0400

    Add: hand-of-straights - submission-0

---
+++ Data Structures & Algorithms/hand-of-straights/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        count = 0
+        maximum = max(hand)
+        freq = [0]*(maximum+1)
+        hand_len = len(hand)
+
+        for num in hand:
+            freq[num] += 1
+
+        print(freq)
+
+        while count < hand_len:
+            start_num = 0
+
+            while not freq[start_num]:
+                start_num += 1
+
+            for num in range(start_num, start_num+groupSize):
+                if num > maximum or not freq[num]:
+                    return False
+                
+                freq[num] -= 1
+                count += 1
+        
+        return True
