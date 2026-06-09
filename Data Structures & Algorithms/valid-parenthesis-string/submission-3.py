0a231ba5b09df25bee2a602a3e2d83844f9567a6
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-06-08 20:52:41 -0400

    Add: valid-parenthesis-string - submission-3

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        
+        lifeline = 0
+        opening = []
+        indices = {}
+
+        for index, char in enumerate(s):
+            if char == "*":
+                lifeline += 1
+            elif char == "(":
+                opening.append(index)
+            else:
+                if not opening:
+                    if not lifeline:
+                        return False
+                    else:
+                        lifeline -= 1
+                else:
+                    opening.pop()
+            
+            indices[index] = lifeline
+
+        #****(*(((
+        print(len(s))
+
+        for i in range(len(opening)-1, -1, -1):
+            index = opening[i]
+            after = lifeline - indices[index]
+
+            if after <= 0:
+                return False
+            
+            lifeline -= 1
+
+        return True
