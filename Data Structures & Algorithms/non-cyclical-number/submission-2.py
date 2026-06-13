a3b94f7fbba07cf15291af0bf018b71a728b095e
Ikechukwu Mgbemele <imgbemele@gmail.com>
2026-06-13 19:19:10 -0400

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        def is_one(n):
+            new_n = 0
+            str_n = str(n)
+
+            for char in str_n:
+                int_char = int(char)
+
+                new_n += int_char ** 2
+            
+            return new_n
+        
+        seen = set()
+
+        while n not in seen:
+            seen.add(n)
+            n = is_one(n)
+            if n == 1:
+                return True
+        
+        return False
